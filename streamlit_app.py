046501f04a0f3abff50f987bb2de6e0c54d79cbe
Anthony Capone <anthony.capone@snowflake.com>
2025-06-20 11:42:10 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 #Convert the Snowpark dataframe to a pandas dataframe
-pd_df = my_dataframe.to_pandas()
+pd_df = my_dataframe.pd.to_pandas()
 st.dataframe(pd_df)
 st.stop()
                                                                       
